563c4b7b36ca21e1d456572175fbec0d105d51e2
telmomenezes <telmo@telmomenezes.com>
2015-11-30 19:30:03 +0100

    orgmode

--- orgmode.el
+++ orgmode.el
@@ -1,3 +1,9 @@
+;; org mode
+(add-hook 'org-mode-hook 'turn-on-font-lock) ; not needed when global-font-lock-mode is on
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
 ;; Set to the location of your Org files on your local system
 (setq org-directory "~/org")
 ;; Set to the name of the file where new notes will be stored
--- user.el
+++ user.el
@@ -81,11 +81,5 @@
          (getenv "PATH")))
 (setq TeX-PDF-mode t)
 
-;; org mode
-(add-hook 'org-mode-hook 'turn-on-font-lock) ; not needed when global-font-lock-mode is on
-(global-set-key "\C-cl" 'org-store-link)
-(global-set-key "\C-ca" 'org-agenda)
-(global-set-key "\C-cb" 'org-iswitchb)
-
 ;; Emacs speaks statistics
 (require 'ess-site)
